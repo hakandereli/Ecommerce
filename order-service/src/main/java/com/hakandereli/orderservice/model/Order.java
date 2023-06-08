db963e4abb96bb25f9b6ab93f0f7f00b2625c7da
hakan dereli <hakandereli28@gmail.com>
2023-06-08 16:20:44 +0300

    change entity import javax to jakarta

--- order-service/src/main/java/com/hakandereli/orderservice/model/Order.java
+++ order-service/src/main/java/com/hakandereli/orderservice/model/Order.java
@@ -1,11 +1,11 @@
 package com.hakandereli.orderservice.model;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.*;
 import java.util.List;
 
 @Entity
