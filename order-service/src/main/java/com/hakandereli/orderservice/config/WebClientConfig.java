e9a61da0671a271381d351f746aa936ab16ab0d8
hakan dereli <hakandereli28@gmail.com>
2023-06-09 12:55:39 +0300

    Added springflux library to order service. Its configuration is defined. Order service and inventory service will be communicated

--- order-service/src/main/java/com/hakandereli/orderservice/config/WebClientConfig.java
+++ order-service/src/main/java/com/hakandereli/orderservice/config/WebClientConfig.java
@@ -1,2 +1,20 @@
-package com.hakandereli.orderservice.config;public class WebClientConfig {
+package com.hakandereli.orderservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+
+/*
+* Yapılandırma Sınıfları: Spring projenizde yapılandırma ayarlarını tutan sınıfları tanımlamak için
+* @Configuration anotasyonunu kullanabilirsiniz. Bu sınıflar genellikle @Bean anotasyonuyla işaretlenmiş metotlar içerir
+* ve Spring konteyneri tarafından yönetilen nesnelerin yapılandırmasını sağlar.
+* */
+@Configuration
+public class WebClientConfig {
+
+    @Bean
+    public WebClient webClient(){
+        return WebClient.builder().build();
+    }
 }
